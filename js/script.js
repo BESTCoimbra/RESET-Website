8e6ac907c4b4fab1a096704f5ab0ec4478f89049
Paulo Meira <75759802+Palmeira01@users.noreply.github.com>
2024-01-29 15:45:58 +0000

    Responsividade da navbar

--- js/script.js
+++ js/script.js
@@ -54,6 +54,31 @@ function openDropdownResponse() {
   }
 }
 
+const navbarPhone = document.querySelector(".options-phone");
+const optionsPhone = document.querySelectorAll('.nav-option2');
+
+var btn = document.querySelector(".btn");
+btn.addEventListener('click', function() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+    this.classList.add('not-active');
+    navbarPhone.style.display = "none";
+  } else {
+    this.classList.remove('not-active');
+    this.classList.add('active');
+    navbarPhone.style.display = "flex";
+  }
+});
+
+optionsPhone.forEach(option => {
+  option.addEventListener('click', function() {
+    console.log('atão');
+    btn.classList.remove('active');
+    btn.classList.add('not-active');
+    navbarPhone.style.display = "none";
+  });
+});
+
 $(function() {
   var originalTop = $("#about-event-prank-button").css('top');
   var originalLeft = $("#about-event-prank-button").css('left');
