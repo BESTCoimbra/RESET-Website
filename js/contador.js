d00f23ab6f9e99b17485373f4c0ed7ac43590d06
Paulo Meira <75759802+Palmeira01@users.noreply.github.com>
2024-02-06 16:58:57 +0000

    Upgrade na HomePage

--- js/contador.js
+++ js/contador.js
@@ -3,21 +3,25 @@ var x = setInterval(function() {
     var now = new Date().getTime();
     var t = deadline - now;
 
-    var totalHours = Math.floor(t / (1000 * 60 * 60));
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
 
     var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((t % (1000 * 60)) / 1000);
 
+    var hoursFormatted = String(hours).padStart(2, '0');
     var minutesFormatted = String(minutes).padStart(2, '0');
     var secondsFormatted = String(seconds).padStart(2, '0');
 
-    if(totalHours < 0){
+    if(days < 0){
+        document.querySelector(".demo-days-p").innerHTML = "00";
         document.querySelector(".demo-hours-p").innerHTML = "00";
         document.querySelector(".demo-minutes-p").innerHTML = "00";
         document.querySelector(".demo-seconds-p").innerHTML = "00";
     }
     else {
-        document.querySelector(".demo-hours-p").innerHTML = totalHours;
+        document.querySelector(".demo-days-p").innerHTML = days;
+        document.querySelector(".demo-hours-p").innerHTML = hoursFormatted;
         document.querySelector(".demo-minutes-p").innerHTML = minutesFormatted;
         document.querySelector(".demo-seconds-p").innerHTML = secondsFormatted;
     }
